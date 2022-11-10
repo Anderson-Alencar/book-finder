49fee54e73a84a40ac71ecf5dd52ce9c13b3e14d
Anderson Alencar <anderson.alencar@dgsolutions.com.br>
2022-11-10 10:37:16 -0300

    feat: add search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Buscador de livros</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from '../components/SearchBar';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Books</h1>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default Home;
