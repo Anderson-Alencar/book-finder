9634cf88773b1d1f7082e7c7e6d4ffb701c887e2
Anderson Alencar <anderson.alencar@dgsolutions.com.br>
2022-11-11 08:46:06 -0300

    feat: show details when click in book

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import BookProvider from './context/books/BookProvider';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,7 +9,9 @@ function App() {
   return (
     <div className="container">
       <BookProvider>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </BookProvider>
     </div>
   );
